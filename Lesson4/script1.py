709f3ad78f57c25afbf38990d514855e58fec198
elize20 <lizusha02@yandex.ru>
2021-12-04 17:45:52 +0300

    Initial project

--- Lesson4/script1.py
+++ Lesson4/script1.py
@@ -7,7 +7,5 @@ for i in range(n):
 for i in range(len(l) - 1):
     for j in range(len(l) - 2, i - 1, -1):
         if l[j] > l[j + 1]:
-            t = l[j]
-            l[j] = l[j + 1]
-            l[j + 1] = t
+            l[j], l[j+1] = l[j+1], l[j]
 print(l)
