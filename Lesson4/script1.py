e1c46516d087573455b20d1ad7b5d0dc60da942d
elize20 <lizusha02@yandex.ru>
2021-12-12 18:53:33 +0300

    Initial project 2

--- Lesson4/script1.py
+++ Lesson4/script1.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
+#Реализовать сортировку списка методом пузырька
+
 n = int(input('Введите кол-во элементов списка = '))
 print('Введите элементы списка (через Enter)')
 l = list()
 for i in range(n):
     l.append(input())
+
 for i in range(len(l) - 1):
     for j in range(len(l) - 2, i - 1, -1):
         if l[j] > l[j + 1]:
-            l[j], l[j+1] = l[j+1], l[j]
+            l[j], l[j + 1] = l[j + 1], l[j]
 print(l)
